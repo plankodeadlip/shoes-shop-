c66410070e9f48df8f1088f59ea2ff4629705e02
plankodeadlip <140203769+plankodeadlip@users.noreply.github.com>
2025-05-11 02:35:09 +0700

    Delete android app directory

--- android app/MoPhoneApp11/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-}
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.3.15");
-    }
-}
